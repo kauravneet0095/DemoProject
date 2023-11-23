7ac5257f6bf5935c60fd47c046956ed702917eb6
Kaur Avneet <kauravneet0095@gmail.com>
2023-11-23 21:56:00 +0530

    Added create notes click listener

--- app/src/main/java/com/example/notesdemo/presentation/createnotes/CreateNotesFragment.kt
+++ app/src/main/java/com/example/notesdemo/presentation/createnotes/CreateNotesFragment.kt
@@ -7,14 +7,16 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.notesdemo.databinding.FragmentCreateNotesBinding
+import com.example.notesdemo.domain.model.NotesEntity
 import com.example.notesdemo.presentation.createnotes.adapter.ColorPaletteAdapter
 import com.example.notesdemo.presentation.createnotes.model.ColorPaletteModel
-import com.example.notesdemo.presentation.notes.model.NotesModel
+import com.example.notesdemo.presentation.notes.viewmodel.NotesViewModel
 import com.example.notesdemo.utils.ColorConstants
 
 class CreateNotesFragment : Fragment() {
     private var binding: FragmentCreateNotesBinding? = null
     private var selectedColor: ColorPaletteModel? = null
+    private var notesViewModel: NotesViewModel? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
@@ -45,14 +47,16 @@ class CreateNotesFragment : Fragment() {
                 Toast.makeText(requireContext(), "fill notes details first", Toast.LENGTH_SHORT)
                     .show()
             } else {
-                val note = selectedColor?.paletteColor?.let { it1 ->
-                    NotesModel(
+                notesViewModel?.addNotes(
+                    NotesEntity(
+                        123,
                         binding?.etTitle?.text.toString(),
                         binding?.etDesc?.text.toString(),
-                        "",
-                        it1
-
+                        selectedColor?.paletteColor.toString(),
+                        false, "", ""
                     )
+                ) { message ->
+                    println(message)
                 }
             }
         }
