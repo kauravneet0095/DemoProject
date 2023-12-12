382ddd514db93d15b56b3053ebb565043dc22be9
Kaur Avneet <kauravneet0095@gmail.com>
2023-12-12 21:29:08 +0530

    added nav graph and solve db bugs

--- app/src/main/java/com/example/notesdemo/MainActivity.kt
+++ app/src/main/java/com/example/notesdemo/MainActivity.kt
@@ -2,35 +2,20 @@ package com.example.notesdemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.Fragment
+import com.example.notesdemo.databinding.ActivityMainBinding
+import com.example.notesdemo.presentation.notes.component.ViewNotesFragment
+import com.example.notesdemo.utils.ExtensionClass.replaceFragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    val viewNotesFragment: Fragment = ViewNotesFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        val navController = findNavController(R.id.nav_host_fragment_activity_bottom_navigation)
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_view_notes,
-                R.id.navigation_create_notes
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(R.id.mainFragment, ViewNotesFragment())
     }
 
-    private inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
-        val fragmentTransaction = beginTransaction()
-        fragmentTransaction.func()
-        fragmentTransaction.commit()
-    }
 }
